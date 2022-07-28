0f73d42eaa1ccb36b7a46baedb0b8ff78b13ab94
huangjiandong <huangjiandong@meideng.net>
2022-07-28 11:59:28 +0800

    MOD: 更新invoke脚本

--- {{cookiecutter.project_name}}/tasks.py
+++ {{cookiecutter.project_name}}/tasks.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
+from datetime import datetime
 
 from invoke import task
 
@@ -8,19 +10,27 @@ root = os.path.dirname(os.path.abspath(__file__))
 package_name = "{{cookiecutter.project_slug}}"
 
 
+def covert_source(source):
+    source = source.replace("/", ".")
+    if source.endswith(".py"):
+        source = source[:-3]
+    return source
+
+
 @task
 def clean(ctx):
-    """清除项目中无效文件
-    """
+    """清除项目中无效文件"""
     ctx.run("rm -rf build dist", echo=True)
     ctx.run("find . -name '*.pyc' -exec rm -f {} +", echo=True)
     ctx.run("find . -name '*.pyo' -exec rm -f {} +", echo=True)
+    ctx.run("find . -name '*.log' -exec rm -f {} +", echo=True)
     ctx.run("find . -name '__pycache__' -exec rm -rf {} +", echo=True)
     ctx.run("find . -name 'htmlcov' -exec rm -rf {} +", echo=True)
-    ctx.run("find . -name '.coverage' -exec rm -rf {} +", echo=True)
+    ctx.run("find . -name '.coverage*' -exec rm -rf {} +", echo=True)
     ctx.run("find . -name '.pytest_cache' -exec rm -rf {} +", echo=True)
     ctx.run("find . -name '.benchmarks' -exec rm -rf {} +", echo=True)
     ctx.run("find . -name '*.egg-info' -exec rm -rf {} +", echo=True)
+    ctx.run("find . -name '.DS_Store' -exec rm -rf {} +", echo=True)
 
 
 @task(clean)
@@ -30,30 +40,42 @@ def sdist(ctx):
 
 @task(clean)
 def upload(ctx, name="private"):
-    """上传包到指定pip源
-    """
+    """上传包到指定pip源"""
     ctx.run("python setup.py sdist upload -r %s" % name, echo=True)
 
 
 @task(sdist)
 def tupload(ctx, name="private"):
-    """上传包到指定pip源
-    """
+    """上传包到指定pip源"""
     ctx.run("twine upload dist/* -r %s" % name, echo=True)
+
+
+@task
+def checkone(ctx, source):
+    """检查代码规范
+
+    inv checkone tasks.py
+    """
+    ctx.run(
+        "isort --check-only --diff {source}".format(source=source),
+        echo=True,
+    )
+    {% if cookiecutter.python_version[0] == "3" %}ctx.run("black --check {source}".format(source=source), echo=True){% endif %}
+    ctx.run("flake8 {source}".format(source=source), echo=True)
+    {% if cookiecutter.python_version[0] == "3" %}ctx.run("mypy {source}".format(source=source), echo=True){% endif %}
 
 
 @task(clean)
 def check(ctx, job=4):
-    """检查代码规范
-    """
-    ctx.run("isort --check-only --diff {{cookiecutter.project_slug}}", echo=True)
-    {% if cookiecutter.python_version[0] == "3" %}ctx.run("black --check {{cookiecutter.project_slug}}", echo=True){% endif %}
-    {% if cookiecutter.use_pylint.lower() in ("y", "yes") %}ctx.run("pylint --rcfile=.pylintrc -j %s --output-format parseable {{cookiecutter.project_slug}}" % job, echo=True)
+    """检查代码规范"""
+    ctx.run("isort --check-only --diff %s --skip" % package_name, echo=True)
+    {% if cookiecutter.python_version[0] == "3" %}ctx.run("black --check %s" % package_name, echo=True){% endif %}
+    {% if cookiecutter.use_pylint.lower() in ("y", "yes") %}ctx.run("pylint --rcfile=.pylintrc -j %s --output-format parseable %s" % package_name % job, echo=True)
     ctx.run("pylint --rcfile=.pylintrc -j %s --output-format parseable tests --ignore=test.py "
             "--disable=C0111,W0201,R0201" % job,
             echo=True)
-    {% else %}ctx.run("flake8 {{cookiecutter.project_slug}}", echo=True){% endif %}
-    ctx.run("mypy {{cookiecutter.project_slug}}", echo=True)
+    {% else %}ctx.run("flake8 %s" % package_name, echo=True){% endif %}
+    {% if cookiecutter.python_version[0] == "3" %}ctx.run("mypy %s" % package_name, echo=True){% endif %}
 
 
 @task(clean)
@@ -69,12 +91,11 @@ def unittest(ctx):
 
 @task(clean)
 def coverage(ctx):
-    """运行单元测试和计算测试覆盖率
-    """
+    """运行单元测试和计算测试覆盖率"""
     ctx.run(
         "export PYTHONPATH=`pwd` && "
-        "coverage run --rcfile=.coveragerc --source={{cookiecutter.project_slug}} -m pytest tests && "
-        "coverage report -m --fail-under=100",
+        "coverage run --rcfile=.coveragerc --source=%s -m pytest tests && "
+        "coverage report -m --fail-under=100" % package_name,
         encoding="utf-8",
         pty=True,
         echo=True,
@@ -90,7 +111,9 @@ def unittestone(ctx, source, test):
     ctx.run(
         "export PYTHONPATH=`pwd` && "
         "pytest -vv -rsxS -q --cov-config=.coveragerc --cov-report term-missing "
-        "--cov --cov-fail-under=100 {source} {test}".format(source=source, test=test),
+        "--cov --cov-fail-under=100 {source} {test}".format(
+            source=covert_source(source), test=test
+        ),
         encoding="utf-8",
         pty=True,
         echo=True,
@@ -106,7 +129,7 @@ def coverageone(ctx, source, test):
     ctx.run(
         "export PYTHONPATH=`pwd` && "
         "coverage run --rcfile=.coveragerc --source={source} -m pytest -vv -rsxS -q {test} && "
-        "coverage report -m".format(source=source, test=test),
+        "coverage report -m".format(source=covert_source(source), test=test),
         encoding="utf-8",
         pty=True,
         echo=True,
@@ -115,8 +138,7 @@ def coverageone(ctx, source, test):
 
 @task(clean)
 def format(ctx):
-    """格式化代码
-    """
+    """格式化代码"""
     autoflake_args = [
         "--remove-all-unused-imports",
         "--recursive",
@@ -125,29 +147,92 @@ def format(ctx):
         "--exclude=__init__.py",
     ]
     ctx.run(
-        "autoflake {args} {{cookiecutter.project_slug}}".format(args=" ".join(autoflake_args)),
+        "autoflake {args} {package_name} tests".format(args=" ".join(autoflake_args)),
+        package_name=package_name,
         echo=True,
     )
-    ctx.run("isort {{cookiecutter.project_slug}}", echo=True)
-    {% if cookiecutter.python_version[0] == "3" %}ctx.run("black {{cookiecutter.project_slug}}", echo=True){% endif %}
+    ctx.run("isort %s tests" % package_name, echo=True)
+    {% if cookiecutter.python_version[0] == "3" %}ctx.run("black %s tests" % package_name, echo=True){% endif %}
 
 
 @task(clean)
 def formatone(ctx, source):
-    """格式化单个文件
-    """
+    """格式化单个文件"""
     autoflake_args = [
+        "--ignore-init-module-imports",
         "--remove-all-unused-imports",
         "--recursive",
         "--remove-unused-variables",
         "--in-place",
+        "--exclude=__init__.py",
     ]
     ctx.run(
-        "autoflake {args} {source}".format(source=source, args=" ".join(autoflake_args)),
+        "autoflake {args} {source}".format(
+            source=source, args=" ".join(autoflake_args)
+        ),
         echo=True,
     )
     ctx.run("isort {source}".format(source=source), echo=True)
     {% if cookiecutter.python_version[0] == "3" %}ctx.run("black {source}".format(source=source), echo=True){% endif %}
+
+
+def get_site_packages_dir(packages: str) -> str:
+    if packages is None:
+        return ""
+    if not os.getenv("VIRTUAL_ENV"):
+        return ""
+    site_packages_dir = subprocess.check_output(
+        "find ${VIRTUAL_ENV} -name 'site-packages'", shell=True, text=True
+    ).strip()
+    if not packages.strip():
+        return site_packages_dir
+    packages_list = [
+        package_name.strip()
+        for package_name in packages.split(",")
+        if package_name.strip()
+    ]
+    packages_unique = sorted(
+        list(set(packages_list)), key=lambda x: packages_list.index(x)
+    )
+    if packages_unique:
+        return " ".join(
+            [
+                os.path.join(site_packages_dir, package_name)
+                for package_name in packages_unique
+            ]
+        )
+    return site_packages_dir
+
+
+def get_plint_args(msg_ids: str, ignore_default: bool) -> list:
+    pylint_args = [
+        "--jobs=0",
+        "--exit-zero",
+        "--persistent=n",
+    ]
+    if not ignore_default:
+        pylint_args.append(
+            "--ignore=.git,venv*,docs,node_modules,tests,test*.py,debug_celery.py"
+        )
+    if msg_ids:
+        pylint_args.extend(["--disable=all", f"--enable={msg_ids}"])
+    return pylint_args
+
+
+@task
+def pylintone(ctx, source, msg_ids="", packages=None, ignore_default=False):
+    """检查单个文件
+
+    inv --help pylintone
+
+    inv pylintone --source '{{cookiecutter.project_slug}} *.py' --msg-ids="W1505" --packages="pymongo,celery"
+    """
+    time_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_name = f'pylint_{msg_ids.replace(",", "_")}_{time_str}.log'
+    site_packages_dir = get_site_packages_dir(packages)
+    pylint_args = " ".join(get_plint_args(msg_ids, ignore_default))
+    cmd = f"pylint {pylint_args} {source} {site_packages_dir} > {file_name}"
+    ctx.run(cmd, echo=True)
 
 
 @task
